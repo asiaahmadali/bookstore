e45c0b861cd83b9c3348dccb4ca815a11dfadb10
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-08-05 01:14:39 +0500

    changing path of img

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,9 @@
 function Home() {
   return (
-    <div className="bg-[url('images/bg.png')] w-full h-screen bg-cover bg-center">
+    <div
+      style={{ backgroundImage: "url('/images/bg.png')" }}
+      className="w-full h-screen bg-cover bg-center"
+    >
       <h1 className="text-center text-2xl md:text-3xl font-medium p-4 pt-14 text-white">
         Welcome To my Book Store App
       </h1>
