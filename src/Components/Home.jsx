722a8327f1f25c033cb9787d034ef230d5bde2b9
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-08-05 00:34:46 +0500

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div className="bg-[url('images/book-bg.png')] w-full h-screen bg-cover bg-center">
+    <div className="bg-[url('images/bg.png')] w-full h-screen bg-cover bg-center">
       <h1 className="text-center text-2xl md:text-3xl font-medium p-4 pt-14 text-white">
         Welcome To my Book Store App
       </h1>
